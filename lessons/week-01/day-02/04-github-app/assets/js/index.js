9db771b63db8293f20af0358f99acbd21c0468c0
surajverma2587 <surajverma2587@gmail.com>
2023-02-15 19:09:06 +0000

    complete code

--- lessons/week-01/day-02/04-github-app/assets/js/index.js
+++ lessons/week-01/day-02/04-github-app/assets/js/index.js
@@ -1,6 +1,9 @@
 // target the form
 const searchForm = document.getElementById("search-form");
 
+// target container for repos
+const githubRepos = document.getElementById("github-repos");
+
 // create event handler function
 const handleSubmit = async (event) => {
   event.preventDefault();
@@ -9,52 +12,78 @@ const handleSubmit = async (event) => {
   const searchInput = document.getElementById("search-input");
   const username = searchInput.value;
 
-  try {
-    // construct URL with username
-    const url = `https://api.github.com/users/${username}/repos`;
+  // get the error element
+  const errorElement = document.getElementById("search-input-error");
 
-    // fetch data from URL
-    const response = await fetch(url);
+  // handle error
+  if (!username) {
+    const errorMessage = "Please enter a valid GitHub username.";
 
-    if (response.status !== 200) {
-      throw new Error("Something went wrong. Please try again!!");
+    errorElement.innerHTML = errorMessage;
+    githubRepos.innerHTML = "";
+  } else {
+    try {
+      // clear any previous errors
+      errorElement.innerHTML = "";
+
+      // construct URL with username
+      const url = `https://api.github.com/users/${username}/repos`;
+
+      // fetch data from URL
+      const response = await fetch(url);
+
+      if (response.status !== 200) {
+        throw new Error("Something went wrong. Please try again!!");
+      }
+
+      const data = await response.json();
+
+      // render repos
+      renderRepos(data);
+    } catch (error) {
+      // handle error
+      renderError(error);
     }
-
-    const data = await response.json();
-
-    // render repos
-    renderRepos(data);
-  } catch (error) {
-    // handle error
-    renderError(error);
   }
 };
 
 const renderRepos = (repos) => {
-  // log repos
-  console.log(repos);
+  // check if repos is empty
+  if (repos.length === 0) {
+    // render no repos
+    const warningComponent = `<div class="alert alert-warning" role="alert">
+      No repositories found. Please try a different username.
+    </div>`;
 
-  // map through repos and for each repo build
-  /**
-   * <div class="card" style="width: 18rem">
-        <div class="card-body">
-          <h5 class="card-title">Repo title</h5>
-          <a href="#" class="card-link">Repo link</a>
-        </div>
+    githubRepos.innerHTML = warningComponent;
+  } else {
+    // construct repo callback function
+    const constructRepo = (repo) => {
+      // create a repo component
+      const repoComponent = `<div class="card" style="width: 18rem">
+      <div class="card-body">
+        <h5 class="card-title">${repo.name}</h5>
+        <a href="${repo.html_url}" class="card-link">Repo link</a>
       </div>
-   */
+    </div>`;
 
-  // append repo to parent container
+      return repoComponent;
+    };
+
+    // map through repos and for each repo build
+    const reposToRender = repos.map(constructRepo).join("");
+
+    // append repo to parent container
+    githubRepos.innerHTML = reposToRender;
+  }
 };
 
 const renderError = (error) => {
-  // log repos
-  console.log(error);
-  /**
-   * <div class="alert alert-danger" role="alert">
-        error
-      </div>
-   */
+  const errorComponent = `<div class="alert alert-danger" role="alert">
+    ${error.message}
+  </div>`;
+
+  githubRepos.innerHTML = errorComponent;
 };
 
 // register the event to the handler function
