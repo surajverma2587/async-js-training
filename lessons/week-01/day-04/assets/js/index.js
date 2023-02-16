0d9f58a83ee23b66307ceb91dd873b1954a5be1c
surajverma2587 <surajverma2587@gmail.com>
2023-02-16 19:20:47 +0000

    wip

--- lessons/week-01/day-04/assets/js/index.js
+++ lessons/week-01/day-04/assets/js/index.js
@@ -1,1 +1,6 @@
-console.log("hello from js");
+const CURRENT_WEATHER_URL =
+  "https://api.openweathermap.org/data/2.5/weather?q=london&appid=60b4fb66103f9e3c6f93920a7d7f1377";
+
+const API_KEY = "8109f605d79877f7488a194794a29013";
+
+const ICON_URL = "http://openweathermap.org/img/w/03n.png";
